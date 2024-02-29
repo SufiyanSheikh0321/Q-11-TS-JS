4ef59fce89e222baf29a0cc26401e516622a1180
Muhammed Sufiyan <148213204+SufiyanSheikh0321@users.noreply.github.com>
2024-02-29 19:56:43 +0500

    Add files via upload

---
+++ Q.11.js
@@ -0,0 +1,7 @@
+"use strict";
+// 11.Names: Store the name of a few of your friends in a array called name. Print each person's name by accessing each element in the list, one at a time.
+let names = ["Sufiyan", "Amir", "Asnaif", "Ashir"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
